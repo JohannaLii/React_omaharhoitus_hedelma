25789a4b38003db2e081ae4ab1bc565a46fadca5
JohannaLii <johanna.liinamaki@edu.keuda.fi>
2025-06-26 13:38:49 +0300

    Lisätty hahmottelu

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-// Otsikko-komponentti
+// Otsikko
 const Otsikko = () => {
-  return <h1 className="header">Tämä on kuvaileva tapa toimia.</h1>;
+  return <h1 className="header">Tietoa Reactista</h1>;
 };
 
-// Navigointipalkki-komponentti
+// Navigointipalkki
 const Navigointi = () => {
   return (
-    <nav>
-      <hi>Johannan oma sivu</hi>
-      <ul className="navigointi-lista" style={{ listStyleType: "disc", paddingLeft: "20px" }}>
-        <li><a href="#koti">Koti</a></li>
-        <li><a href="#meista">Meistä</a></li>
-        <li><a href="#yhteydenotto">Ota yhteyttä</a></li>
+        <nav style={{ backgroundColor: '#ccc', padding: '10px' }}>
+      <ul style={{ 
+        listStyle: 'none', 
+        display: 'flex', 
+        gap: '20px', 
+        margin: 0, 
+        padding: 0
+        }}>
+      <li style={{ cursor: 'pointer' }}>Koti</li>
+        <li style={{ cursor: 'pointer' }}>Meistä</li>
+        <li style={{ cursor: 'pointer' }}>Ota yhteyttä</li>
       </ul>
     </nav>
   );
 };
 
-// Sisalto-komponentti
+// Sisalto
 const Sisalto = () => {
-  return <h2>Sisältö</h2>;
+  return (
+   <main style={{ backgroundColor: '#FFF9C4', padding: '25px' }}>
+    <h1>Tietoa Reactista</h1>
+      <ul>
+        <li>Julkaistiin ensimmäisen kerran 2013</li>
+        <li>Alunperin Jordan Walken luomus</li>
+        <li>Reactilla on yli 100K tähteä Gitissä</li>
+        <li>On Facebookin ylläpitämä</li>
+        <li>Toimii moottorina tuhansille ohjelmille, myös mobiilisoftille</li>
+      </ul>
+    </main>
+   );
 };
 
-// Alatunniste-komponentti
+// Alatunniste
 const Alatunniste = () => {
   return (
-    <footer>
-      <p>Copyright Jyri Lindroos 2024</p>
+     <footer style={{ backgroundColor: 'green', color: 'white', textAlign: 'center', padding: '5px' }}>
+      <p>© Jyri Lindroos 2024, kaikki oikeudet pidätetään</p>
     </footer>
   );
 };
@@ -35,6 +51,11 @@ const Alatunniste = () => {
 const App = () => {
   return (
     <div>
+        <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="Reactin logo"
+        style={{ width: "40px", display: "block", marginBottom: "15px" }}
+        />
       <Otsikko />
       <Navigointi />
       <Sisalto />
@@ -46,5 +67,6 @@ const App = () => {
 // Renderöidään App root-elementtiin
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
 
 
